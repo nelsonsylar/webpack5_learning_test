cab28516f57a45660a0a91268a56275831c57bc4
yangning xiao <a2135825@163.com>
2021-07-10 12:02:12 +0800

    feat: webpack管理输入输出

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import _ from 'lodash'
+import printMe from './print'
 function component() {
   var element = document.createElement('div');
+  var btn = document.createElement('button')
 
   // Lodash通过npm包以es6的模块方式引入
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
 
+  element.appendChild(btn);
   return element;
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,12 @@
 const path = require('path')
 //webpack是属于node环境,所以默认不支持es6的import,需要使用require
 module.exports = {
-  entry:'./src/index.js',
+  entry:{
+    app:'./src/index.js',
+    print:'./src/print.js'
+  },
   output:{
-    filename:'bundle.js',
+    filename: '[name].bundle.js', // [name]为entry的key,如entry为app,则filename为app.bundle.js
     path: path.resolve(__dirname,'dist') // webpack涉及到文件地址的情况,均需要使用path,__dirname在node里表示,当前文件所对应的文件夹位置
     //而 path.resolve(__dirname,'文件夹名字')是一个固定套路,得到当前目录的dist文件夹位置
   }
