956d6f19f80f52907e106279bdd201b9a4ce55e4
yangning xiao <a2135825@163.com>
2021-07-12 11:10:49 +0800

    feat: 如果在两个js都引入loadash而不优化的话,两个js打包过后都很大

--- src/index.js
+++ src/index.js
@@ -1,18 +1,4 @@
-import { cube } from './math.js';
-
-if (process.env.NODE_ENV !== 'production') {
-  console.log('Looks like we are in development mode!');
-}
-
-function component() {
-  var element = document.createElement('pre');
-
-  element.innerHTML = [
-    'Hello webpack!',
-    '5 cubed = ' + cube(5)
-  ].join('\n\n');
-
-  return element;
-}
-
-document.body.appendChild(component());
+import _ from 'lodash';
+console.log(
+  _.join(['Main', 'index', 'loaded!'], '啦啦啦')
+);
--- webpack.dev.js
+++
@@ -1,8 +0,0 @@
-const {merge} = require('webpack-merge'); // 使用webpack-merge options
-const common = require('./webpack.common.js');
-module.exports = merge(common, {
-  devtool: 'inline-source-map',
-  devServer: {
-    contentBase: './dist'
-  }
-});
