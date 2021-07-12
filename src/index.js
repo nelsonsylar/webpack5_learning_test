e8e53cc3b4c85bf2c7a26a7f88dd862c1398cad9
yangning xiao <a2135825@163.com>
2021-07-12 14:17:48 +0800

    feat: import的条件加载组件

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
-async function getComponent() {
-  // 这里使用es6的import函数promise返回default export object
+import _ from 'lodash';
+function component() {
   var element = document.createElement('div');
-  const _ = await import(/* webpackChunkName: "lodash" */ 'lodash');
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  var button = document.createElement('button');
+  var br = document.createElement('br');
+
+  button.innerHTML = 'Click me and look at the console!';
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.appendChild(br);
+  element.appendChild(button);
+
+// Note that because a network request is involved, some indication
+// of loading would need to be shown in a production-level site/app.
+  button.onclick = e => import(/* webpackChunkName: "print" */ './print').then(module => {
+    var print = module.default;  //使用import()函数拿到的module要使用default才能拿到对应的export整体
+    print();
+  });
+
   return element;
 }
-getComponent().then(component => {
-  document.body.appendChild(component);
-})
+
+document.body.appendChild(component());
