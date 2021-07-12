b6f16ee516c7c58dee3852e167ebb247f7d62ada
yangning xiao <a2135825@163.com>
2021-07-12 17:33:29 +0800

    feat: 在webpack中使用typescript

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,25 @@
 const path = require('path');
-const webpack = require('webpack');
-const WorkboxPlugin = require('workbox-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin') 
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
-  entry: {
-    index:'./src/index.js',
-    polyfills: './src/polyfills.js'
+  entry: './src/index.ts',
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ] // 导入时可省去后缀名
   },
   output: {
-    filename: '[name].bundle.js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
   plugins:[
-    new HtmlWebpackPlugin({
-      title: 'Progressive Web Application'
-    }),
-    new WorkboxPlugin.GenerateSW({
-      // 这些选项帮助 ServiceWorkers 快速启用
-      // 不允许遗留任何“旧的” ServiceWorkers
-      clientsClaim: true,
-      skipWaiting: true
-    })
+    new HtmlWebpackPlugin({title:'typescript in webpack'})
   ]
 };
