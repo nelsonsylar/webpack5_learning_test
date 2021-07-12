07224dbf71ed00731974d5a2d2f4197568632cc7
yangning xiao <a2135825@163.com>
2021-07-12 11:40:59 +0800

    feat: 使用import函数和webpackChunkName来动态加载脚本

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
-import _ from 'lodash';
-console.log(
-  _.join(['Main', 'index', 'loaded!'], '啦啦啦')
-);
+async function getComponent() {
+  // 这里使用es6的import函数promise返回default export object
+  var element = document.createElement('div');
+  const _ = await import(/* webpackChunkName: "lodash" */ 'lodash');
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  return element;
+}
+getComponent().then(component => {
+  document.body.appendChild(component);
+})
--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,18 @@
 const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
-const webpack = require('webpack')
+
 module.exports = {
   entry: {
     index: './src/index.js',
-    another: './src/another-module.js'
   },
   plugins: [
     new HTMLWebpackPlugin({
-      title: 'Code Splitting'
+      title: '动态导入'
     }),
-    // new webpack.optimize.CommonsChunkPlugin({ //公共引入的包的设置   以被弃用,使用optimization
-    //     name: 'common' // 指定公共 bundle 的名称。
-    // })
   ],
   output: {
     filename: '[name].bundle.js',
+    chunkFilename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
-  optimization:{
-    splitChunks:{
-      cacheGroups:{
-        commons :{
-          name:"common",
-          chunks:'initial',
-          minChunks:2
-        }
-      }
-    }
-  }
 };
