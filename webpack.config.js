bbe1607de52f12d14033e312273d4690781b5bb6
yangning xiao <a2135825@163.com>
2021-07-12 11:19:48 +0800

    feat: 使用splitChunks代替CommonsChunkPlugin导出公共引入的包

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
-
+const webpack = require('webpack')
 module.exports = {
   entry: {
     index: './src/index.js',
@@ -9,10 +9,24 @@ module.exports = {
   plugins: [
     new HTMLWebpackPlugin({
       title: 'Code Splitting'
-    })
+    }),
+    // new webpack.optimize.CommonsChunkPlugin({ //公共引入的包的设置   以被弃用,使用optimization
+    //     name: 'common' // 指定公共 bundle 的名称。
+    // })
   ],
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist')
+  },
+  optimization:{
+    splitChunks:{
+      cacheGroups:{
+        commons :{
+          name:"common",
+          chunks:'initial',
+          minChunks:2
+        }
+      }
+    }
   }
 };
